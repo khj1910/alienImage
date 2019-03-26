801d17a0d28b66c8ff146e0afcf51ab59fb6644c
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-03-26 15:45:52 +0900

    ChangeAlien

--- ImageView03/ViewController.swift
+++ ImageView03/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var cheak: UILabel!
+    var count = 1
+    var qkdgid = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "frame1.png")
+        
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+        if count==5{
+            qkdgid=false
+        } else if count==1{
+            qkdgid=true
+        }
+        if qkdgid==true{
+            count+=1
+        }else{
+            count-=1
+        }
+        
+        
+        
+        myImageView.image = UIImage(named: "frame\(count).png")
+        
+        cheak.text = String(count)
+    }
+    
 }
 
